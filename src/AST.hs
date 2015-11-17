16b6d160a4065fec8e0872efab0210d3c265e564
John Cowie <jcowie@thoughtworks.com>
2015-11-17 19:40:02 +0000

    Can now create anonymous function

--- src/AST.hs
+++ src/AST.hs
@@ -46,7 +46,6 @@ instance Show Symbol where
 symbolToInteger :: Symbol -> Either EvalError Integer
 symbolToInteger (Literal i) = Right i
 symbolToInteger (Function _ _) = Left "can't convert function to integer"
-symbolToInteger (SymbolName _) = Left "can't convert symbol to integer"
 
 readInteger :: Scope -> Symbol -> Either EvalError Integer
 readInteger scope a = (evalAST scope a) >>= symbolToInteger
@@ -54,17 +53,38 @@ readInteger scope a = (evalAST scope a) >>= symbolToInteger
 readIntegers :: Scope -> [Symbol] -> Either EvalError [Integer]
 readIntegers scope = sequence . map (readInteger scope)
 
-anonFunction :: Scope -> [Symbol] -> Either EvalError Symbol
-anonFunction scope = fmap last . sequence . map (evalAST scope)
+-- anonFunction :: Scope -> [Symbol] -> Either EvalError Symbol
+-- anonFunction scope [] = Left "anonymous function must have arg list"
+-- anonFunction scope (argList:args) =
 
-storeFunction :: Scope -> [Symbol] -> Either EvalError Symbol
-storeFunction scope args = Right (Function "an anonymous function" anonFunction)
+-- fun is called with an arglist
+
+bindArgToScope :: Scope -> String -> Symbol -> Scope
+bindArgToScope scope sym v = Map.insert sym v scope
+
+bindArgsToScope :: Scope -> [Symbol] -> [Symbol] -> Either EvalError Scope
+bindArgsToScope scope [] [] = Right scope
+bindArgsToScope scope ((SymbolName s):ans) (ag:ags) =
+  bindArgsToScope (bindArgToScope scope s ag) ans ags
+bindArgsToScope scope (an:ans) (ag:ags) = Left "Args must be symbols"
+bindArgsToScope _ _ _ = Left "Not the right number of args"
+
+func :: [Symbol] -> [Symbol] -> Scope -> [Symbol] -> Either EvalError Symbol
+func argNames body scope args = do
+  newScope <- bindArgsToScope scope argNames args
+  result <- sequence (map (evalAST newScope) body)
+  return (last result)
+
+
+createFunction :: Scope -> [Symbol] -> Either EvalError Symbol
+createFunction scope [] = Left "anonymous function must have arg list"
+createFunction scope ((List argList):body) = Right $ Function "anonymousFunction" (func argList body)
 
 globalScope :: Map.Map String Symbol
 globalScope = Map.fromList [  ("+", Function "+" plus)
                             , ("-", Function "-" minus)
                             , ("*", Function "*" mult)
-                            , ("fun", Function "fun" storeFunction)
+                            , ("fun", Function "fun" createFunction)
                         ]
 
 lookupSymbol :: Map.Map String Symbol -> String -> Either EvalError Symbol
@@ -125,12 +145,12 @@ readAST s = case (pt . tokens) s of  -- FIXME this case statement is broken
   Right (ast, n) -> Left "Mismatched parentheses"
   Left err -> Left err
 
-resolveSymbolName :: Symbol -> Either EvalError Symbol
-resolveSymbolName (SymbolName s) = lookupSymbol globalScope s
-resolveSymbolName symbol = Right symbol
+resolveSymbolName :: Scope -> Symbol -> Either EvalError Symbol
+resolveSymbolName scope (SymbolName s) = lookupSymbol scope s
+resolveSymbolName scope symbol = Right symbol
 
 resolveFunction :: Scope -> Symbol -> Either EvalError Symbol
-resolveFunction scope a = (evalAST scope a) >>= resolveSymbolName
+resolveFunction scope a = (evalAST scope a) >>= (resolveSymbolName scope)
 
 execFunction :: Scope -> Symbol -> [Symbol] -> Either EvalError Symbol
 execFunction scope (Function _ f) args = f scope args
@@ -166,7 +186,7 @@ evalAST localScope (List []) = Left "empty expression"
 evalAST localScope (List (n:ns)) = do
   f <- resolveFunction localScope n
   execFunction localScope f ns
-evalAST localScope v = Right v
+evalAST localScope v = resolveSymbolName localScope v
 
 eval :: String -> Either EvalError Symbol
 eval s = (readAST s) >>= (evalAST globalScope)
